e24736bc2b692dd111dc6048b07a82a374275f94
Yuliia <dikun.yulia@gmail.com>
2023-01-03 00:03:26 +0200

    add hero media

--- src/js/swipper.js
+++ src/js/swipper.js
@@ -3,23 +3,28 @@ import Swiper, { Autoplay, Navigation, Pagination } from 'swiper';
 Swiper.use([Navigation, Pagination, Autoplay]);
 
 const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    direction: 'horizontal',
-    loop: true,
-    centeredSlides: true,
-    autoplayDisableOnInteraction: false,
-    speed: 300,
-    slidesPerView: 1,
-    initialSlide: 0,
-    spaceBetween: 0,
-  
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-      type: 'bullets',
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  centeredSlides: true,
+  autoplayDisableOnInteraction: false,
+  speed: 300,
+  slidesPerView: 1,
+  initialSlide: 0,
+  spaceBetween: 0,
+
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    type: 'bullets',
+  },
+  autoplay: {
+    delay: 2000,
+    disableOnInteraction: false,
+  },
+  breakpoints: {
+    1440: {
+      direction: 'vertical',
     },
-    autoplay: {
-        delay: 2000,
-        disableOnInteraction: false,
-      },
-  });
+  },
+});
