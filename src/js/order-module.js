a13ef8047deb414fc1a6d408b4f2d7c3f1b3c206
Veronica <vveronikanika@gmail.com>
2023-01-03 15:13:07 +0200

    fix(order layout) change font family

--- src/js/order-module.js
+++ src/js/order-module.js
@@ -1,15 +1,14 @@
-import templatePreOrder from '../templates/pre-order.hbs';
-// // import templateOrderNow from '../templates/order-now.hbs';
+// import templatePreOrder from '../templates/pre-order.hbs';
+import templatePreOrder from '../templates/order-now.hbs';
 
 //Харкод об'єкта з цінами, які будуть приходити з адмінки.
 const objectWithPrices = {
-  oldPrice: 1499,
-  price: 995,
+  price: 1499,
+  salePrice: 995,
 };
 
 //Харкод створення актуального рядка розмітки з об'єктом цін
 const template = templatePreOrder(objectWithPrices);
-console.log(template);
 
 getOrderSection(template);
 
