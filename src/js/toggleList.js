32b4df0a46d7b5b3ed619770125dac929650c04b
egarra <garikhabuzov@gmail.com>
2023-01-03 12:25:50 +0200

    script fix, showing first checkbox list in desktop

--- src/js/toggleList.js
+++ src/js/toggleList.js
@@ -3,15 +3,21 @@ export const toggleList = () => {
     const checkBoxList = document.querySelectorAll('.howitworks-checkbox__list')
     const arrowIcons = document.querySelectorAll('.howitworks__arrow-icon')
 
+    if(window.innerWidth >= 1440) {
+        checkBoxList[0].classList.remove('visually-hidden')
+    } else {
+        checkBoxList[0].classList.add('visually-hidden')
+    }
+
     items.forEach((item, i) => {
         item.addEventListener('click', function() {
             if(this) {
-               const style = window.getComputedStyle(checkBoxList[i]).display;
-               style === 'none' ? checkBoxList[i].style.display = 'flex' : checkBoxList[i].style.display = 'none';
+               checkBoxList[i].classList.toggle('visually-hidden')
                arrowIcons[i].classList.toggle('rotate')
             }
         })
     })
 }
 
+window.addEventListener("resize", toggleList);
 toggleList();
