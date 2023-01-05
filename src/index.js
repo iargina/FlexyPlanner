e568f9e589bcba735e5b23a53d2e7a33ee330ffc
egarra <garikhabuzov@gmail.com>
2023-01-05 13:51:48 +0200

    order button fix, modal pre order connect

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import { toggleList } from './js/toggleList.js';
-/* import { toggleModal } from './js/toggleModal.js'; */
 import './js/swipper.js';
 import './js/order-module.js';
+import './js/toggleModal.js'
