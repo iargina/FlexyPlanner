e16284c21865c135d1cf9708e5b48cf054f97c14
egarra <garikhabuzov@gmail.com>
2023-01-07 16:09:21 +0200

    Update toggleModal.js

--- src/js/toggleModal.js
+++ src/js/toggleModal.js
@@ -1,7 +1,12 @@
- export default function toggleModal (triggerBtn, closeBtn, backdrop) {
+export default function toggleModal(triggerBtn, closeBtn, backdrop) {
+  
   const openModalBtn = document.querySelector(triggerBtn),
     closeModalBtn = document.querySelector(closeBtn),
     backdropSection = document.querySelector(backdrop);
+
+    if (!openModalBtn) {
+      return;
+    }
 
   openModalBtn.addEventListener('click', () => {
     backdropSection.classList.toggle('modal-is-hidden');
@@ -16,4 +21,10 @@
       backdropSection.classList.toggle('modal-is-hidden');
     }
   });
-};
+
+  window.addEventListener('keydown', (e) => {
+    if (e.code === 'Escape') {
+      backdropSection.classList.add('modal-is-hidden');
+    }
+  });
+}
