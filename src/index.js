74fcac36ec0c68e7023d88dbf0c3dab9871696b9
Svitlana Synytsia <svetlankogr@gmail.com>
2023-01-09 23:13:23 +0100

    successPayment

--- src/index.js
+++ src/index.js
@@ -2,4 +2,5 @@ import './js/swipper.js';
 import './js/order-module.js';
 import './js/animation.js';
 import './js/button-up.js';
+import './js/successPayment.js';
 import { toggleList } from './js/toggleList.js';
