fa0c2536eb6d234c13a01e1236b88f3c6551b260
egarra <garikhabuzov@gmail.com>
2023-01-09 18:24:17 +0200

    preloader add

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import './js/swipper.js';
 import './js/order-module.js';
 import './js/animation.js';
 import './js/button-up.js';
-import './js/onPreOrderFormSubmit.js'
+import './js/onPreOrderFormSubmit.js';
+import './js/preLoader.js';
 import { toggleList } from './js/toggleList.js';
