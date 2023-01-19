a9be3451afc3c6b8e14a42d17a78b7d679593e4d
z9877969 <z9877969@ukr.net>
2023-01-20 00:48:08 +0200

    calcs hero paddings

--- src/js/heroVh.js
+++ src/js/heroVh.js
@@ -120,15 +120,42 @@ const heroMarkup = `<section class="hero" id="hero__up">
 </section>
 `;
 heroWrapper.innerHTML = heroMarkup;
+// function centeredHero() {
+//   const hero = document.querySelector('.hero');
+//   const heroHeight = hero.offsetHeight;
+
+//   const padding = heroWrapperHeight - heroHeight;
+//   console.log(padding);
+//   if (padding > 0) {
+//     hero.style.paddingTop = `${padding / 2}px`;
+//     hero.style.paddingBottom = `${padding / 2}px`;
+//   }
+// }
 function centeredHero() {
   const hero = document.querySelector('.hero');
   const heroHeight = hero.offsetHeight;
 
   const padding = heroWrapperHeight - heroHeight;
-  console.log(padding);
+  // console.log(padding);
   if (padding > 0) {
-    hero.style.paddingTop = `${padding / 2}px`;
-    hero.style.paddingBottom = `${padding / 2}px`;
+    const { pB, pT } = calcP();
+    hero.style.paddingTop = `${pT}px`;
+    hero.style.paddingBottom = `${pB}px`;
   }
 }
 centeredHero();
+
+console.log(window.screen.height - window.innerHeight);
+
+function calcP() {
+  const containerH = document.querySelector('.hero-container').clientHeight;
+  const screenH = window.screen.height;
+  const viewPortH = window.innerHeight;
+  const pannelH = screenH - viewPortH;
+  const p = (screenH - containerH) / 2;
+
+  return {
+    pT: p - pannelH,
+    pB: p + pannelH,
+  };
+}
