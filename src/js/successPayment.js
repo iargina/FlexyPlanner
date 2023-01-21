729f06bd509d23ddfe2071573e90d72f0ee6660e
Irina <iargina.irina@gmail.com>
2023-01-21 19:22:26 +0200

    successPayment fixed

--- src/js/successPayment.js
+++ src/js/successPayment.js
@@ -1,7 +1,9 @@
 import { parseOrder } from './services/query-methods';
 const closeModalBtn = document.querySelector('.close-btn');
 const backdropSection = document.querySelector('.success');
-
+const urlParams = new URLSearchParams(window.location.search);
+const buyer = urlParams.get('buyer');
+const products = urlParams.get('products');
 /* npm start */
 
 function openSuccessModal() {
@@ -15,29 +17,9 @@ backdropSection.addEventListener('click', e => {
     backdropSection.classList.toggle('is-hidden');
   }
 });
-const testSearchByOrder = search => {
-  return (
-    /(\bsource_id\b)+/.test(search) &&
-    /(\bsource_uuid\b)+/.test(search) &&
-    /(\bbuyer\b)+/.test(search) &&
-    /(\bshipping\b)+/.test(search) &&
-    /(\bproducts\b)+/.test(search) &&
-    /(\bpayments\b)+/.test(search)
-  );
-};
 
-window.onload = function () {
-  const { search } = location;
-  if (search !== '' && testSearchByOrder(search)) {
-    let orderBody = parseOrder(search);
-    return orderBody;
-  }
-};
-
-async function successPayment() {
-  const order = await window.onload();
-
-  if (order) {
+function successPayment() {
+  if (buyer && products) {
     openSuccessModal();
   }
   return;
