24ac07c9bd68ad6c02caf64c8264e2ae754e5e30
Veronica <vveronikanika@gmail.com>
2023-02-14 12:52:46 +0200

    style: change positioning in footer

--- src/js/footer.js
+++ src/js/footer.js
@@ -2,10 +2,10 @@ import { btnLoader } from './btnLoader';
 
 const getButton = () => {
   return `
-
+<div class="footer-btn-wrapper">
   <a href="#order" class="btn btn--primary footer__button">
 			${btnLoader}
-  </a>`;
+  </a></div>`;
 };
 
 const footerSection = document.querySelector('.footer__section');
